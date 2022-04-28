91a368082253c3a9b9e4097dd61aa97a16dab623
gabi939 <42301421+gabi939@users.noreply.github.com>
2022-04-28 09:57:53 +0300

    kwak

---
+++ service.py
@@ -0,0 +1,52 @@
+import json
+import os
+from flask import Flask,request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/api/resource", methods=['GET', 'POST'])
+def service():
+
+    if request.method == 'POST':
+        data = request.data
+
+        try:
+            os.remove("/tmp/temp.json")
+        except Exception:
+            pass
+
+        with open("/tmp/temp.json", "w+") as file:
+            json.dump(data,file)
+
+        requests.post(url="35.184.239.203/api/update",data=data)
+        return 200
+
+    else:
+
+        try:
+            with open("/tmp/temp.json", "r") as file:
+                data = json.load(file)
+            return data
+
+        except Exception:
+            return 500
+
+
+
+@app.route("/api/update")
+def service_update():
+
+    data = request.data
+
+    try:
+        os.remove("/tmp/temp.json")
+    except Exception:
+        pass
+
+    with open("/tmp/temp.json", "w+") as file:
+        json.dump(data, file)
+
+
+app.run(host="34.121.14.255")
